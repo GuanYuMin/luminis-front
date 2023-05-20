ffad6cbd4afa2c2ac0853e49e3629f7e73c365dc
jtabordave <jtaborda@gmail.com>
2023-05-20 14:47:24 -0300

    Agregado registro en boton registrate de home

--- src/app/shared/components/landing-third-section/landing-third-section.component.ts
+++ src/app/shared/components/landing-third-section/landing-third-section.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbModalRef, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { UserService } from 'app/shared/services/user.service';
+import { ToastaService, ToastaConfig, ToastOptions, ToastData } from 'ngx-toasta';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-third-section',
@@ -7,11 +11,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingThirdSectionComponent implements OnInit {
   logged_in: boolean = false;
+  register_response: any;
+  public loading: boolean = false;
+  registerForm: FormGroup;
+
+  constructor(
+    private modalService: NgbModal,
+    private userService: UserService,
+    private toastaService: ToastaService,
+    private toastaConfig: ToastaConfig,
+    private fb: FormBuilder
+    ) {
+        this.toastaConfig.theme = 'bootstrap';
+        this.toastaConfig.position = 'top-right';
+        this.createForm();
+    }
+
+    createForm() {
+      this.registerForm = this.fb.group({
+         nombres: ['', [Validators.required, Validators.minLength(2)]],
+         apellidos: ['', [Validators.required, Validators.minLength(2)]],
+         nombre_usuario: ['', [Validators.required, Validators.minLength(2)]],
+         telefono: ['', [Validators.required, Validators.minLength(10)]],
+         email_registro: ['', [Validators.required, Validators.email]],
+         password_registro: ['', [Validators.required, Validators.minLength(12)]],
+         password_registro2: ['', [Validators.required, Validators.minLength(12), this.ComparePassword('password_registro')]]
+      });
+    }
+
+    ComparePassword(field_name): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } => {
+          const input = control.value;
+          const isSame = control.root.value[field_name] == input;
+          return !isSame ? {'isSame': {isSame}}: null;
+        };
+    }
 
   ngOnInit(): void {
-      let token = localStorage.getItem("token") || ""
-      if (token.length > 0) {
-        this.logged_in = true;
-      }
+    let token = localStorage.getItem("token") || ""
+    if (token.length > 0) {
+      this.logged_in = true;
     }
+  }
+
+  openRegisterModal(registercontent: any) {
+    let ngbModalOptions: NgbModalOptions = {
+          backdrop : 'static',
+          keyboard : false
+    };
+    this.modalService.open(registercontent, ngbModalOptions);
+  }
+
+  doRegister(nombres: string, apellidos: string, nombre_usuario: string, telefono: string, email: string, password: string, password2: string) {
+    this.loading = true;
+    this.userService.fn_CreateUser({
+        email: email,
+        password: password,
+        username: nombre_usuario,
+        name: nombres,
+        midlename: "",
+        lastname: apellidos,
+        birthdate: "1984-11-27T16:09:01.057Z",
+        phone: telefono,
+        role_id: 2,
+        photo_id: 1
+    }).subscribe((res) => {
+      this.loading = false;
+      if (res.status == 200) {
+        this.modalService.dismissAll();
+        this.showSuccess("Registro", res.message);
+      } else {
+        this.showError("Registro", "Ha ocurrido un error");
+      }
+    }, (err) => {
+      if (err.error.message) {
+        this.showError("Registro", err.error.message);
+      } else {
+        this.showError("Registro", err.message);
+      }
+      this.loading = false;
+    });
+  }
+
+  showError(title: string, message: string) {
+      var toastOptions:ToastOptions = {
+          title: title,
+          msg: message,
+          showClose: true,
+          timeout: 5000,
+          theme: 'bootstrap'
+      };
+      this.toastaService.error(toastOptions);
+  }
+
+  showSuccess(title: string, message: string) {
+      var toastOptions:ToastOptions = {
+          title: title,
+          msg: message,
+          showClose: true,
+          timeout: 5000,
+          theme: 'bootstrap'
+      };
+      this.toastaService.success(toastOptions);
+  }
 }
+
